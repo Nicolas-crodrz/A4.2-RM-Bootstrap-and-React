5a7a8164e25c0551e0e9f8479d95c128e5af5ac0
Nicolas <73233224+Nicolas-Cabrera-Rodriguez@users.noreply.github.com>
2023-01-21 18:59:28 +0000

    Implementacion de jquery 2/3, en todos los .html

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
-
+// Carrusel de imagenes
 $(document).ready(function(){
   $('.headerContenido').slick({
-  /*slidesToShow: 3,
-  dots:true,
-  centerMode: true,*/
-
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   });
 });
+
+
+// Typing
+new TypeIt(".typing", {
+  speed: 100,
+  waitUntilVisible: true,
+}).go();
